ec386b858321a6ede90df45621bfb603eb887eb4
Amir Hossein Baghernezhad <amhoba2014@gmail.com>
2025-07-02 20:16:02 +0000

    added integration adn frontend

---
+++ frontend/assets/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# # Run the development server
+# echo "Starting development server..." ;
+# pnpm i ;
+# pnpm dev &
+
+# Keep the script running indefinitely
+echo "Running indefinitely..." ;
+sleep infinity &
+
+wait $! ;
