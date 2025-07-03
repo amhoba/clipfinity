d612602632d5aaba253aa95ef8f1b0ae74afac94
Amir Hossein Baghernezhad <amhoba2014@gmail.com>
2025-07-03 09:35:51 +0000

    frontend entrypoint

--- frontend/assets/entrypoint.sh
+++ frontend/assets/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# # Run the development server
-# echo "Starting development server..." ;
-# pnpm i ;
-# pnpm dev &
+# Run the development server
+echo "Starting development server..." ;
+pnpm i ;
+pnpm dev &
 
 # Keep the script running indefinitely
 echo "Running indefinitely..." ;
