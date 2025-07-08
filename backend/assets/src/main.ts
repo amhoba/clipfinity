c93929593ef29070b562811283b060cc9425fc06
Amir Hossein Baghernezhad <amhoba2014@gmail.com>
2025-07-08 18:22:28 +0000

    utilized cookie parser for backend

--- backend/assets/src/main.ts
+++ backend/assets/src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  app.use(cookieParser());
+
   const port = process.env.PORT || 3000;
   const host = process.env.HOST || '0.0.0.0';
-  
+
   await app.listen(port, host);
-  
+
   console.log(`Application is running on: http://${host}:${port}`);
 }
 bootstrap();
