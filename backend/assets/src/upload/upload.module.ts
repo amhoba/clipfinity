6851919bbf53bfc5de1804c2ebb199a30d578698
Amir Hossein Baghernezhad <amhoba2014@gmail.com>
2025-07-12 13:23:55 +0000

    added minio service

--- backend/assets/src/upload/upload.module.ts
+++ backend/assets/src/upload/upload.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
+import { MinioService } from '../minio/minio.service';
 
 @Module({
     controllers: [UploadController],
-    providers: [UploadService],
+    providers: [UploadService, MinioService],
 })
 export class UploadModule { }
