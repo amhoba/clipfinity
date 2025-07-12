9bfb749763f32e172688362327a880f3ddd5daa9
Amir Hossein Baghernezhad <amhoba2014@gmail.com>
2025-07-12 14:17:30 +0000

    create user when logged in

--- backend/assets/src/users/users.controller.ts
+++ backend/assets/src/users/users.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from '../auth/user.decorator';
+import { ClerkUserDTO } from 'src/auth/dto/clerkuser.dto';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
 
     @Get('profile')
-    async getProfile(@User() user: any) {
-        return this.usersService.findOrCreate(user.clerk_hash);
+    async getProfile(@User() user: ClerkUserDTO) {
+        return this.usersService.findOrCreate(user.sub);
     }
 
     @Get(':id')
