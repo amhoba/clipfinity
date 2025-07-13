8d9e87e142b7ea18e2418c2e34f8d06d379cb08c
Amir Hossein Baghernezhad <amhoba2014@gmail.com>
2025-07-13 16:42:54 +0000

    profile changed

--- backend/assets/src/users/users.controller.ts
+++ backend/assets/src/users/users.controller.ts
@@ -9,11 +9,7 @@ export class UsersController {
 
     @Get('profile')
     async getProfile(@User() user: ClerkUserDTO) {
-        return this.usersService.findOrCreate(user.sub, {
-            email: user.email,
-            first_name: user.given_name,
-            last_name: user.family_name
-        });
+        return this.usersService.findOrCreate(user.sub);
     }
 
     @Get(':id')
