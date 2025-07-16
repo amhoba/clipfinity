f4adb0abe3101fa3a2d6e963b5254c3988e0e5ea
Amir Hossein Baghernezhad <amhoba2014@gmail.com>
2025-07-16 15:59:35 +0000

    updated integration scripts

--- integration/do.py
+++ integration/do.py
@@ -13,9 +13,8 @@ from compose import setup_typer_app
 project_name = "clipfinity"
 
 app = typer.Typer(no_args_is_help=True)
-app.add_typer(setup_typer_app(f"docker compose -p {project_name}_dev -f docker-compose.yml"), name="dev")
-app.add_typer(setup_typer_app(f"docker compose -p {project_name}_staging -f docker-compose.yml"), name="staging")
-app.add_typer(setup_typer_app(f"docker compose -p {project_name}_prod -f docker-compose.yml"), name="prod")
+app.add_typer(setup_typer_app(f"docker compose -p {project_name}_dev -f docker-compose.dev.yml"), name="dev")
+app.add_typer(setup_typer_app(f"docker compose -p {project_name}_prod -f docker-compose.prod.yml"), name="prod")
 
 
 if __name__ == "__main__":
